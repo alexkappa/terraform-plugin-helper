d6bdef9c49c593fab12ed8eb85983c62e104a200
Alex Kalyvitis <alex.kalyvitis@gmail.com>
2020-03-26 12:17:54 +0100

    wip using map

--- helper/flatten/flatten.go
+++ helper/flatten/flatten.go
@@ -1,28 +1,122 @@
 package flatten
 
+import (
+	"fmt"
+	"reflect"
+)
+
 // A Flattener is used to flatten data into Terraform's internal representation.
 type Flattener interface {
-	Flatten() map[string]interface{}
+	Flatten(map[string]interface{})
 }
 
 // The FlattenerFunc type is an adapter to allow the use of an ordinary function
 // as a Flattener. If f is a function with the appropriate signature,
 // FlattenerFunc(f) is a Flattener that calls f.
-type FlattenerFunc func() map[string]interface{}
+type FlattenerFunc func(map[string]interface{})
 
-// Flatten calls f().
-func (fn FlattenerFunc) Flatten() map[string]interface{} {
-	return fn()
+// Flatten calls f(m).
+func (fn FlattenerFunc) Flatten(m map[string]interface{}) {
+	fn(m)
 }
 
 // Flatten executes the provided flatteners Flatten method and wraps the result
 // in a []interface{} which is used by Terraform list or set types.
 func Flatten(f Flattener) []interface{} {
-	return []interface{}{f.Flatten()}
+	m := make(map[string]interface{})
+	f.Flatten(m)
+	return []interface{}{m}
 }
 
 // FlattenFunc executes the provided function and wraps the result in a
 // []interface{} which is used by Terraform list or set types.
-func FlattenFunc(fn func() map[string]interface{}) []interface{} {
-	return []interface{}{Flatten(FlattenerFunc(fn))}
+func FlattenFunc(fn func(map[string]interface{})) []interface{} {
+	return Flatten(FlattenerFunc(fn))
 }
+
+func FlattenList(in interface{}) (out []interface{}) {
+
+	switch reflect.TypeOf(in).Kind() {
+	case reflect.Slice, reflect.Array:
+		s := reflect.ValueOf(in)
+
+		for i := 0; i < s.Len(); i++ {
+			m := make(map[string]interface{})
+			f, ok := s.Index(i).Interface().(Flattener)
+			if !ok {
+				panic(fmt.Sprintf("FlattenList: in[%d] %s is not a Flattener", i, s.Index(i).Type()))
+			}
+			f.Flatten(m)
+			out = append(out, m)
+		}
+
+	default:
+		panic("FlattenList: input is not a slice or array type")
+	}
+
+	return
+}
+
+func FlattenListFunc(in interface{}, fn func(interface{}, map[string]interface{})) (out []interface{}) {
+
+	switch reflect.TypeOf(in).Kind() {
+	case reflect.Slice, reflect.Array:
+
+		s := reflect.ValueOf(in)
+
+		for i := 0; i < s.Len(); i++ {
+			m := make(map[string]interface{})
+			fn(s.Index(i).Interface(), m)
+			out = append(out, m)
+		}
+	default:
+		panic("FlattenListFunc: input is not a slice or array type")
+	}
+
+	return
+}
+
+type FlattenerList []Flattener
+
+func List(in interface{}) FlattenerList {
+
+	var fl []Flattener
+
+	v := reflect.ValueOf(in)
+
+	switch v.Kind() {
+	case reflect.Slice, reflect.Array:
+
+		fl = make([]Flattener, v.Len())
+		for i := 0; i < v.Len(); i++ {
+			f, ok := v.Index(i).Interface().(Flattener)
+			if !ok {
+				panic(fmt.Sprintf("FlattenList: in[%d] %s is not a Flattener", i, v.Index(i).Type()))
+			}
+			fl[i] = f
+		}
+
+	default:
+		panic("FlattenList: input is not a slice or array type")
+	}
+
+	return FlattenerList(fl)
+}
+
+// ---
+
+// flatten.ListFunc(in.ContainerSpec.Mounts, func(v interface{}, m map[string]interface{} {
+// 	m["target"] = v.(mount.Mount).Target
+// 	m["source"] = v.(mount.Mount).Source
+// 	m["type"] = v.(mount.Mount).Type
+// }))
+
+// flatten.List(in.ContainerSpec.Mounts).FlattenFunc(func(v interface{}, m map[string]interface{}){
+// 		m["target"] = v.(mount.Mount).Target
+// 		m["source"] = v.(mount.Mount).Source
+// 		m["type"] = v.(mount.Mount).Type
+// })
+
+// func List() Iterator
+
+// Iterator.Flatten()
--- helper/flatten/flatten_test.go
+++ helper/flatten/flatten_test.go
@@ -2,18 +2,89 @@ package flatten
 
 import "testing"
 
-type flattener struct{}
+type flattener struct {
+	foo string
+}
 
-func (f flattener) Flatten() map[string]interface{} {
-	return map[string]interface{}{"foo": "bar"}
+func (f flattener) Flatten(m map[string]interface{}) {
+	m["foo"] = f.foo
 }
 
 func TestFlatten(t *testing.T) {
-	Flatten(flattener{})
+	flat := Flatten(flattener{"bar"})
+	t.Logf("%v", flat) // [map[foo:bar]]
 }
 
 func TestFlattenFunc(t *testing.T) {
-	FlattenFunc(func() map[string]interface{} {
-		return map[string]interface{}{"foo": "bar"}
+	flat := FlattenFunc(func(m map[string]interface{}) {
+		m["foo"] = "bar"
 	})
+	t.Logf("%v", flat) // [map[foo:bar]]
+}
+
+func TestFlattenList(t *testing.T) {
+	flatteners := []interface{}{
+		flattener{"bar"},
+		flattener{"baz"},
+	}
+	flat := FlattenList(flatteners)
+	t.Logf("%v", flat) // [map[foo:bar] map[foo:baz]]
+}
+
+type item struct{ name string }
+
+type itemFlattener item
+
+func (i itemFlattener) Flatten(m map[string]interface{}) {
+	m["name"] = i.name
+}
+
+func TestFlattenListWrap(t *testing.T) {
+	flatteners := []interface{}{
+		itemFlattener(item{"bar"}),
+		itemFlattener(item{"baz"}),
+	}
+	flat := FlattenList(flatteners)
+	t.Logf("%v", flat) // [map[name:bar] map[name:baz]]
+}
+
+func itemFlattenerAlt(i item) Flattener {
+	return FlattenerFunc(func(m map[string]interface{}) {
+		m["name"] = i.name
+	})
+}
+
+func TestFlattenListWrapAlt(t *testing.T) {
+	flatteners := []interface{}{
+		itemFlattenerAlt(item{"bar"}),
+		itemFlattenerAlt(item{"baz"}),
+	}
+	flat := FlattenList(flatteners)
+	t.Logf("%v", flat) // [map[name:bar] map[name:baz]]
+}
+
+func TestFlattenListFunc(t *testing.T) {
+	items := []item{{"foo"}, {"bar"}}
+	flat := FlattenListFunc(items, func(i interface{}, m map[string]interface{}) {
+		m["name"] = i.(item).name
+	})
+	t.Logf("%v", flat)
+}
+
+func TestFlattenNested(t *testing.T) {
+	type bar struct{ baz string }
+	type foo struct{ bar *bar }
+
+	v := &foo{&bar{"hey!"}}
+
+	flat := FlattenFunc(func(m map[string]interface{}) {
+		m["bar"] = FlattenFunc(func(m map[string]interface{}) {
+			m["baz"] = v.bar.baz
+		})
+	})
+	t.Logf("%v", flat)
+}
+
+func TestList(t *testing.T) {
+
 }
